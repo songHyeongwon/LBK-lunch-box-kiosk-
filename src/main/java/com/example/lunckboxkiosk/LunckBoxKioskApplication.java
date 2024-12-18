4050c8ebeabeeeac24c7d61240e51277d7f18821
bumin <vvxxxxk@gmail.com>
2024-12-18 10:02:49 +0900

    chore: initialize project

---
+++ src/main/java/com/example/lunckboxkiosk/LunckBoxKioskApplication.java
@@ -0,0 +1,12 @@
+package com.example.lunckboxkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunckBoxKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunckBoxKioskApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/lunckboxkiosk/LunckBoxKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lunckboxkiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunckBoxKioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
