176d2490105b76a19d0b88df3d29021397c54f7d
bumin <vvxxxxk@gmail.com>
2024-12-20 10:46:47 +0900

    feat: Mail 관련 dependency 및 설정 파일 추가

---
+++ src/main/java/com/example/lunchboxkiosk/common/config/MailConfig.java
@@ -0,0 +1,52 @@
+package com.example.lunchboxkiosk.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender mailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = getJavaMailProperties();
+        mailSender.setJavaMailProperties(props);
+
+        return mailSender;
+    }
+
+    private static Properties getJavaMailProperties() {
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "false");
+        props.put("mail.smtp.ssl.trust", "smtp.naver.com");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        return props;
+    }
+}
